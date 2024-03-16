609c4039f02dd4321c32cbda0ee2b1445bc0de76
mihail-gosman <mihailc.gosman@gmail.com>
2024-03-16 13:51:16 +0200

    qa-simple-demo
    Simple /auth/register/organization  test

---
+++ tests/test_authentication.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+import requests
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://127.0.0.1:8000")
+
+    payload = {
+        "username": "lep[",
+        "email": "string",
+        "password": "string",
+        "organization_name": "string",
+        "hq_address": "string"
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post("http://127.0.0.1:8000/auth/register/organization", json=payload, headers=headers)
+
+    response_json = response.json()
+
+    assert response_json["message"] == "Organization registration successful."
+
+    print("Organization registration test passed!")
+
+finally:
+    # Close the browser
+    driver.quit()
