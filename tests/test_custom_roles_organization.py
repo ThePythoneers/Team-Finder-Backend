c3ba9f90fd6ecce3e673ab2490cc2f747e291e81
Daniel P <147325072+dani238qr@users.noreply.github.com>
2024-03-16 16:16:06 -0700

    Custom roles-Get Organization Info Test

---
+++ tests/test_custom_roles_organization.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+import requests
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://127.0.0.1:8000")
+
+    payload ={
+        "detail": [
+            {
+            "loc": [
+                "string",
+                0
+            ],
+            "msg": "string",
+            "type": "string"
+            }
+        ]
+    }
+
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post("http://127.0.0.1:8000/organization/", json=payload, headers=headers)
+
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert "organization_info" in response_json
+
+    print("Get Organization Info test passed!")
+
+except AssertionError:
+    print("Get Organization Info test failed!")
+
+
+finally:
+    driver.quit()
