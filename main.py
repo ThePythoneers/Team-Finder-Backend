55792e290bb3af363b88e988e1a47ed93a268be5
Connor Dechart <gboca6@gmail.com>
2024-03-17 20:45:22 +0200

    bug fix

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ if __name__ == "__main__":
             f"{colorama.Fore.GREEN}DATABASE: {colorama.Fore.WHITE}Database reset, created primary roles."
         )
 
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
